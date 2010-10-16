541c2df2ac497112569f20d950c782ed553f714b
scott@zeddic.com <scott@zeddic.com@052f6fd5-1608-f3fb-c693-1316d95f1d4d>
2010-10-16 00:35:04 +0000

    Removes java files that were incorrectly added to the bin directory

--- trunk/src/com/zeddic/game/common/particle/ParticleData.java
+++
@@ -1,9 +0,0 @@
-package com.zeddic.game.common.particle;
-
-/**
- * Custom data that will be provided to every particle when it is emitted. This
- * allows emitter builders to supply data to all the particles by subclassing.
- */
-public class ParticleData {
-
-}
